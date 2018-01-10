2eba31489564f1798a4082f81a530057711a74bb
Marzena <marzena_szopinska@hotmail.com>
2018-01-10 19:19:26 +0000

    added form, picture upload to fix

--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class CreateComponent extends Component {
+
+    state = {
+        picture: "https://freeiconshop.com/wp-content/uploads/edd/person-flat.png"
+    }
+
+    addContact = (e) => {
+        // prevent the full page refresh on submit
+        e.preventDefault();
+    };
+
     render() {
         return (
             <div>
-            Create Contact!!!!!!!!!!!!!!!!
+                <Link to="/" className="close-create-contact">Close</Link>
+                <form className="create-contact-form" onSubmit={this.addContact}>
+                    <img src={this.state.picture} alt="avatar" />
+                    <input type="file" name="pic" accept="image/*"/>
+                    <input type="text" name="name" placeholder="full name" />
+                    <input type="text" name="email" placeholder="email" />
+                    <input type="text" name="number" placeholder="phone number" />
+                    <input type="text" name="description" placeholder="description" />
+                </form>
+
             </div>
         );
     }
